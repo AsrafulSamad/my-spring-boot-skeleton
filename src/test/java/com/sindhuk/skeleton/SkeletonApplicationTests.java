cafb69fe807469e4f8d32d091e8a69a118472030
Asraful Samad Chowdhury <asraful.chowdhury@pathao.com>
2022-12-22 01:24:34 +0600

    - adding sample base project which has been created from spring initializer.

---
+++ src/test/java/com/sindhuk/skeleton/SkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sindhuk.skeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
